3e615bdd3b838ffe20d25389aabd6db88e4c2046
Mauro Tello <maurotello73@gmail.com>
2021-11-24 22:49:03 -0300

    form refresh

--- src/Form.js
+++ src/Form.js
@@ -2,26 +2,16 @@ import './App.css';
 import CryptoJS from 'crypto-js';
 import React, { useState } from 'react';
 
-class Form extends React.Component {
+const Form = () => {
+
+    const [name, setName] = useState("");
+    const [dni, setDni] = useState("");
+    const [archivo , setArchivo] = useState('');
     
-    state = {
-        selectedFile: null
-      };
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+    const handleChange = (e) => {
     }
-   
-  
-    handleChange(event) {
-        const input = document.querySelector('input[type="file"]');
-        const file2 = input.files[0];
-        this.state.selectedFile = file2;
-        //console.log(this.state.selectedFile);
-    }
-    fileData = () => {
+    
+    const fileData = () => {
         if (this.state.selectedFile1) {
             //console.log(this.state.selectedFile1);
             return (
@@ -41,64 +31,78 @@ class Form extends React.Component {
         }
     };
   
-    handleSubmit(event) {
-        event.preventDefault();
-        console.log("SUBIENDO");
+    const guardarDatos = (e) => {
+        e.preventDefault()
+        console.log(archivo);
+
+        /*
+        if(!name.trim()){
+            console.log('esta vacio nombre')
+            return
+        }
+        
+        if(!dni.trim()){
+            console.log('esta vacio DNI')
+            return
+        }
+        */
+       
         let fileReader = new FileReader();
-        fileReader.readAsBinaryString(this.state.selectedFile);
+        fileReader.readAsBinaryString(archivo);
         fileReader.onload = function () {
             console.log("Result: " + fileReader.result);
         };
         fileReader.onerror = function () {
             console.log("Error: " + fileReader.error);
         };
+
+        console.log('SHA: ' + CryptoJS.SHA256(archivo));
+
+        e.target.reset()
+        setDni('')
+        setName('')
+        setArchivo('')
+
+        console.log("SUBIENDO");
         
-        console.log('SHA: ' + CryptoJS.SHA256(fileReader));
-        //console.log('SHA2: ' + CryptoJS.SHA256(event.target.files[0]));
-        
-        
-        const formData = new FormData();
-        formData.append(
-            "myFile",
-            this.state.selectedFile,
-            this.state.selectedFile.name,
-        );
-        
+        //const formData = new FormData();
+        //formData.append(
+        //    "myFile",
+        //    this.state.selectedFile,
+        //    this.state.selectedFile.name,
+        //);
     }
-  
-    render() {
         return (
             <div>
                 <div class="container mx-auto mt-20 flex w-full clear-both">
-                    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full clear-both" enctype="multipart/form-data" onSubmit={this.handleSubmit}>
+                    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full clear-both" enctype="multipart/form-data" onSubmit={guardarDatos}>
                         <div class="block mb-4">
                             <label for="dni">
                                 DNI
                             </label><br />
-                            <input class="rounded text-black-500 form-input" type="number" name="dni" placeholder="Ingrese su DNI sin puntos ni espacios" />
+                            <input class="rounded text-black-500 form-input" type="number" name="dni" onChange={ (e) => setDni(e.target.value) } placeholder="Ingrese su DNI sin puntos ni espacios" />
                             
                         </div>
                         <div class="block mb-4">
-                            <label for="nombre">
+                            <label for="name">
                                 Nombre
                             </label><br />
-                            <input class="rounded text-black-500 form-input" type="text" name="nombre" placeholder="Ingrese si nombre" />
+                            <input class="rounded text-black-500 form-input" type="text" name="name" onChange={ (e) => setName(e.target.value) } placeholder="Ingrese si nombre" />
                         </div>
                         <div class="block mb-4">
                             <label for="file">
                                 File:
-                                <input class="rounded text-blackf-500" placeholder="Seleccione un archivo" in="file" name="file" type="file" value={this.state.value} onChange={this.handleChange} />
+                                
+                                <input class="rounded text-blackf-500" placeholder="Seleccione un archivo" in="file" name="file" type="file"  onChange={(e)=>{setArchivo(e.target.files[0])}} />
                             </label>
                         </div>
                         <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" value="Submit" />
                     </form>
                 </div>
                 <div class="container mx-auto mt-5 flex w-full clear-both">
-                    {this.fileData()}
+                    
                 </div>
             </div>
-            
-        );
-    }
+        )
 }
 export default Form;
